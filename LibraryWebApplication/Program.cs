42078c5e1c98122a45e931dbca43223fe311e528
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2024-02-18 17:09:21 +0200

    Added ConnectionString

--- LibraryWebApplication/Program.cs
+++ LibraryWebApplication/Program.cs
@@ -1,7 +1,15 @@
+using LibraryWebApplication;
+using LibraryWebApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DbAndInformationSystemsContext>(option => option.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
